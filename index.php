08ab26884779260bca97bc80134eaaf67b1fd058
Giuditta <giudittamarino89@gmail.com>
2021-04-30 16:36:34 +0200

    header terminata

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="it" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="./assets/css/master.css">
+    <title>Google FAQ</title>
+  </head>
+
+  <body>
+    <!-- HEADER -->
+    <header>
+      <div class="header-top">
+        <div class="left">
+          <img src="./assets/img/google_logo.png" alt="google_logo_text">
+          <h3>Privacy e termini</h3>
+        </div>
+        <div class="right">
+          <i class="fas fa-th"></i>
+          <img src="./assets/img/g_google.png" alt="google-logo-g">
+        </div>
+      </div>
+
+      <div class="header-bottom">
+        <nav>
+          <ul class="no-style inline">
+            <li><a href="#" class="normal">Introduzione</a></li>
+            <li><a href="#" class="normal">Norme sulla privacy</a></li>
+            <li><a href="#" class="normal">Termini di servizio</a></li>
+            <li><a href="#" class="normal">Tecnologie</a></li>
+            <li><a href="#" class="normal">Domande frequenti</a></li>
+          </ul>
+        </nav>
+      </div>
+      <hr>
+    </header>
+    <!-- MAIN -->
+    <main>
+      <div class="container">
+        
+      </div>
+    </main>
+    <!-- FOOTER -->
+    <footer>
+
+    </footer>
+  </body>
+</html>
