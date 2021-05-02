255c79a3e2bfb1b19920d132c8299359917bf1d0
Giuditta <giudittamarino89@gmail.com>
2021-05-02 20:18:41 +0200

    stampa nella ul

--- index.php
+++ index.php
@@ -63,26 +63,7 @@ $faqs = [
     ]
   ]
 ];
-
-// var_dump($faqs);
-
-foreach ($faqs as $key => $faq) {
-  // var_dump($faq);
-
-  foreach ($faq as $keytext => $text) {
-    if (is_array($text)) {
-      foreach ($text as $keyqa => $qa) {
-        echo $qa . '<br>';
-      }
-    } else {
-      echo '<strong>' . $text . '</strong>' . '<br>';
-    }
-    // var_dump($text);
-  }
-}
-
  ?>
-
 
 <!DOCTYPE html>
 <html lang="it" dir="ltr">
@@ -126,7 +107,29 @@ foreach ($faqs as $key => $faq) {
     <!-- MAIN -->
     <main>
       <div class="container">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <ul class="no-style">
+        <?php
+
+        // var_dump($faqs);
+        foreach ($faqs as $key => $faq) {
+          // var_dump($faq);
+          foreach ($faq as $keytext => $text) { ?>
+
+
+            <?php
+            if (is_array($text)) {
+              foreach ($text as $keyanswer => $answer) { ?>
+                <?= '<li class= "answer">' . $answer . '</li>';
+              }
+            } else {  ?>
+              <?=  '<li class= "question">' . $text . '</li>';
+            }
+            // var_dump($text);
+          }
+        } ?>
+
+        </ul>
+        <p>ciao</p>
       </div>
     </main>
     <!-- FOOTER -->
